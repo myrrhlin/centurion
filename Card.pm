cae7a697e9206d027d2cf871b3a442fc0a3384ca
Michael Hamlin <1197072+myrrhlin@users.noreply.github.com>
2022-02-21 02:01:42 -0500

    working classes for Card and game state

---
+++ Card.pm
@@ -0,0 +1,171 @@
+package Card;
+
+use 5.26.0;
+use warnings;
+use feature 'signatures', 'postderef';
+
+=head1 Card
+
+A card in the game.  Generally, its C<cost> and C<benefit> fields are
+strings of the letters [YGBP], each representing a cube of the color
+Yellow Green Blue or Pink.  During instantiation, though, it is acceptable
+to use lowercase, or digits 1-4 to represent the colors instead;
+1=Yellow 2=Green 3=Blue 4=Pink
+
+Exceptions to the above:
+* A reward type card has a benefit value of an integer preceded by a + sign.
+* A generic upgrade card has a cost value of '__', representing two cubes
+of arbitrary color.
+
+Aside from the usual way, a card can be instantiated from a
+single arrayref that follows some conventions;
+
+* an arrayref with two elements represent the cost and benefit respectively.
+e.g. [113, 44] swaps 2Y+1B for 2P
+* an arrayref with one element represents the benefit (there was no cost).
+e.g. [11] just gives two yellow
+* a card which upgrades an arbitrary color has a _ char for cost, and 1.
+e.g. [__ => 11] upgrades any two cubes 1 level
+* a reward card has an integer point value for the benefit, with a leading +
+e.g. [11133 => '+9'] pay 3Y+2B for 9 points
+
+=cut
+
+use Carp;
+use Scalar::Util qw( blessed );
+use Data::Printer;
+use List::Util qw( sum );
+
+use Moo;
+use Types::Standard qw( Str Int Enum ArrayRef Maybe InstanceOf );
+
+no warnings 'experimental';
+use namespace::clean;
+
+# coerce card cost/benefit strings into normal representation
+sub norm ($val) {
+   if (ref $val eq 'ARRAY') {
+     my $new = [map {norm($_)} @$val];
+     return $new;
+   } elsif ($val =~ /^_+$/) {
+      # don't touch- this is match any color symbol
+   } elsif ($val =~ /^\+\d+$/) {
+      # don't touch- this is point value for reward card
+   } elsif ($val eq '') {
+     return $val;
+   } elsif (!ref $val) {
+     $val =~ tr/ygbpYGBP/1-41-4/; # make numeric
+     $val = join '', sort {$a <=> $b} split //, $val;  # sort digits ascending
+     $val =~ tr/1-4/YGBP/; # make letters
+   } else {
+     croak "invalid type";
+   }
+   return $val;
+}
+
+# test whether a scalar could be coerced into a Card
+sub cardlike ($val) {
+  return unless my $type = ref $val;
+  return 1 if blessed $val && $val->isa('Card');
+  return if blessed $val || $type ne 'ARRAY';
+  return if @$val > 2 || !@$val;
+  return if grep ref, @$val;  # all elements must be plain scalars
+  return if grep !/^(?:[ygbp1234]+|__|\+\d+)$/i, @$val;
+  return 1;
+}
+
+my %cubeval = (Y => 1, G => 2, B => 3, P => 4);
+
+sub value ($string) {
+  croak "value operates only on cubestrings" if ref $string;
+  return 0 if $string eq '';
+  return $cubeval{$string} if length $string == 1;
+  my %cnt;
+  $cnt{$_}++ for split //, $string;
+  my $value = 0;
+  $value += sum map { $cubeval{$_}*$cnt{$_} } keys %cnt if %cnt;
+  return $value;
+}
+# sorting cubestrings by their value (ascending)
+sub byvalue { value($a) <=> value($b) }
+
+
+# increment a color -- used by conversion card
+sub colorinc ($cube, $plus = 1) {
+  my $colors = 'YGBP';
+  my $current = index($colors, $cube);
+  croak "not a color: $cube" if $current < 0;
+  return 'P' if $current + $plus > 3;
+  return substr($colors, $current+$plus, 1);
+}
+
+has type => (is => 'ro', required => 1,
+  isa => Enum[qw(reward xform)], default => 'xform',
+);
+has cost => (is => 'ro', required => 1, isa => Str, coerce => \&norm);
+has benefit => (is => 'ro', required => 1, isa => Str, coerce => \&norm);
+has gain => (is => 'lazy', isa => Maybe[Int]);
+
+sub _build_gain ($self) {
+  return if $self->type eq 'reward';
+  return 2 if $self->cost eq '__';
+  return value($self->benefit) - value($self->cost);
+}
+
+around BUILDARGS => sub {
+  my ( $orig, $class, @args ) = @_;
+  if (@args == 1) {
+    my $onearg = $args[0];
+    if (ref $onearg eq 'ARRAY') {
+      my $val = norm($args[0]);
+      unshift @$val, '' if @$val == 1;  # single element -- no cost
+      my ($cost, $benefit) = @$val;
+      @args = (cost => $cost, benefit => $benefit);
+      push @args, type => 'reward' if $benefit =~ /^\+/;
+    } elsif (blessed $onearg && $onearg->isa('Card')) {
+      my $card = $onearg;
+      @args = (type => $card->type, cost => $card->cost, benefit => $card->benefit);
+    } else {
+      croak "got unexpected one arg: ".np($onearg);
+    }
+  }
+  return $class->$orig(@args);
+};
+
+sub is_conversion ($self) { $self->cost =~ /^_+$/ }
+sub conversion_list ($self, $cubestring) {
+  croak 'not conversion card?' unless $self->is_conversion;
+  my (%cubes, %result);
+  $cubes{$_}++ for split //, $cubestring;
+  my (@cost, @menu);
+  # combinatorics... on the costs
+  my @colors = sort byvalue keys %cubes;
+  pop @colors if $colors[-1] eq 'P';  # its a waste to convert P
+  while (@colors) {
+    my $color1 = shift @colors;
+    push @cost, $color1 x2 if $cubes{$color1} > 1;
+    push @cost, $color1 . $_ for @colors;
+  }
+  foreach my $cost (@cost) {
+    my $result = join '', map {colorinc($_)} split //, $cost;
+    push @menu, sprintf '%s->%s', $cost, $result;
+  }
+  foreach my $color (sort byvalue keys %cubeval) {
+    last if $color eq 'P';
+    next unless $cubes{$color};
+    push @menu, sprintf '%s->%s', $color, colorinc($color,2);
+    $menu[-1] .= ' BAD!' if $color eq 'B';
+  }
+  return @menu;
+}
+
+sub describe ($self, $width = 0) {
+  my $tag = substr($self->type, 0, 1);
+  $tag .= '+' . $self->gain if $self->type eq 'xform';
+  my $out = sprintf '%s:%s->%s', $tag, $self->cost, $self->benefit;
+  if (length $out < $width) { $out .= ' 'x($width - length $out) }
+  return $out;
+}
+
+
+1;
---
+++ GState.pm
@@ -0,0 +1,226 @@
+package GState;
+
+use 5.26.0;
+use warnings;
+use feature 'signatures', 'postderef';
+
+use Carp;
+use Carp::Always;
+use Data::Printer;
+use Path::Tiny;
+use JSON::MaybeXS;
+
+use Moo;
+use Types::Standard qw( Str Int Enum ArrayRef Object InstanceOf );
+use Card;
+
+no warnings 'experimental';
+use namespace::clean;
+
+use MooX::Clone;
+
+sub _cardify {
+  my @refs;
+  if (@_>1 || Card::cardlike($_[0])) { @refs = @_ }
+  elsif (ref $_[0] eq 'ARRAY') {
+    @refs = grep {Card::cardlike($_)} @{$_[0]};
+    croak "cant cardify something here" unless @refs == @{$_[0]};
+  } else {
+    croak "cant cardify a non arrayref!";
+  }
+  my @cards = map {Card->new($_)} @refs;
+  return wantarray? @cards : \@cards;
+}
+
+# player attributes:
+has score => (is => 'rw', isa => Int, default => 0);
+has cubes => (is => 'rw', isa => Str, required => 1, coerce => \&Card::norm);
+has [qw(hand discard)] => (is => 'rw', required => 1,
+  isa => ArrayRef[InstanceOf["Card"]],
+  coerce => sub {[_cardify( @_ )]},
+  default => sub {[_cardify( [__=>11],[11] )]},  # starting hand
+);
+
+# these normally have 5 cards, but some could be unknown in a future state
+has [qw(reward market)] => (is => 'rw', required => 1,
+  isa => ArrayRef[InstanceOf["Card"], 1, 5] );
+has market_cubes => (is => 'rw', required => 1, isa => ArrayRef[Str, 5, 5],
+  coerce => \&Card::norm, default => sub {[('')x5]} );
+
+# dont need decks if we're not playing a full game
+has deck => (is => 'rw', isa => ArrayRef[InstanceOf["Card"]]);
+has reward_deck => (is => 'rw', isa => ArrayRef[InstanceOf["Card"]]);
+
+around BUILDARGS => sub {
+  my ( $orig, $class, @args ) = @_;
+  my %args;
+  if (@args % 2 == 0) { %args = @args }
+  elsif (@args != 1) { croak 'new takes a hash or hashref only' }
+  elsif (ref $args[0] eq 'HASH') { %args = %{$args[0]} }
+  elsif (blessed $args[0] && $args[0]->isa('GState')) {
+    # %args = %{$args[0]};  # ugly dereference
+    croak 'use clone method instead';
+  } else { croak 'new takes a hash or hashref only' }
+
+  foreach my $att (qw<hand discard market reward>) {
+    next unless $args{$att} && ref $args{$att} eq 'ARRAY';
+    my @cards = _cardify(  # build cards from arrayref
+      map {$att eq 'reward' ? [$_->[0], "+".$_->[1]] : $_}  # insert plus sign on reward cards
+      $args{$att}->@* );  # arrayrefs we start with
+    $args{$att} = \@cards;
+  }
+  $args{cubes} = Card::norm($args{cubes});
+  return $class->$orig(%args);
+};
+
+sub scored ($self, $points) { $self->score( $self->score + $points) }
+
+# sortign cards by usefulness; conversions last
+sub by_use {
+  return 1 if $a->is_conversion;
+  return -1 if $b->is_conversion;
+  Card::value($a->cost) <=> Card::value($b->cost);
+}
+sub sort_hand ($self) {
+  my @c = sort by_use grep { $self->playable($_) } $self->hand->@*;
+  push @c, sort by_use grep { !$self->playable($_) } $self->hand->@*;
+  $self->hand(\@c);
+}
+sub spend ($self, $amount) {
+  my (%cubes, %cost);
+  $cubes{$_}++ for split //, $self->cubes;
+  $cost{$_}++ for split //, $amount;
+  foreach my $color (keys %cost) {
+    $cubes{$color} -= $cost{$color};
+    croak if $cubes{$color} < 0;
+  }
+  $self->cubes(join '', map {$_ x $cubes{$_}} keys %cubes);
+}
+sub earn ($self, $amount) {
+  $self->cubes($amount . $self->cubes);
+}
+
+sub can_play ($self) {
+  return grep {$self->playable($_)} $self->hand->@*;
+}
+sub can_claim ($self) {
+  return grep {$self->playable($_)} $self->reward->@*;
+}
+sub playable ($self, $card) {
+  my (%cubes, %cost);
+  $cubes{$_}++ for split //, $self->cubes;
+  return 1 if %cubes && $card->is_conversion;
+  $cost{$_}++ for split //, $card->cost;
+  foreach my $color (keys %cost) {
+    return 0 if (($cubes{$color}//0) < $cost{$color});
+  }
+  return 1;
+}
+
+sub report ($self) {
+  my @menu;
+  if (my @c = $self->can_claim) {
+    say 'can NOW claim ',scalar(@c),' cards: ', join ', ', map {$_->describe} @c;
+  }
+  print 'Rewards: ';
+  my $num = $self->reward->@*;
+  for my $i (1..$num) {
+    my $card = $self->reward->[$i-1];
+    my $bonus = $i == 1 ? '***' : $i == 2 ? '*' : '';
+    print $i,$bonus,':',$card->describe(13); print $i==$num? "\n" : '  ';
+    push @menu, ["Claim reward $i$bonus: ". $card->describe, $i-1] if $self->playable($card);
+  }
+  my @mktmenu;
+  print 'Market:  ';
+  $num = $self->market->@*;
+  for my $i (1..$num) {
+    my $card = $self->market->[$i-1];
+    my $mcubes = $self->market_cubes->[$i-1];
+    my $bonus = $mcubes ? "+$mcubes" : '';
+    print $i,$bonus,':',$card->describe(13); print $i==$num? "\n" : '  ';
+    next unless $i-1 <= length $self->cubes;
+    my $mincost = substr($self->cubes, 0, $i-1);
+    my $netcost = Card::value($mcubes) - Card::value($mincost);
+    my $bad = $netcost < -2 ? ' BAD!': '';
+    $mincost &&= " spending $mincost";
+    $mincost ||= ' for free';
+    $netcost = "+$netcost" unless $netcost <0;
+    $mincost .= " (net $netcost)";
+    push @mktmenu, ["Purchase market $i$bonus: ". $card->describe . $mincost . $bad, $i-1];
+  }
+  $self->sort_hand;
+  #say "\nHand: ", join '  ', map {$_->describe} $self->hand->@*;
+  if (my @c = $self->can_play) {
+    #say 'can play ',scalar(@c),' cards: ', join ', ', map {$_->describe} @c;
+  }
+  print "\nHand: ";
+  $num = $self->hand->@*;
+  for my $i (1 .. $num) {
+    my $card = $self->hand->[$i-1];
+    my $playable = $self->playable($card) ? '>' : '';
+    # print "\n         Rest: " unless $playable;
+    # print $card->describe; print $i==$self->hand->@* ? "\n" : '  ';
+    print $playable,$card->describe; print $i==$num ? "\n" : '  ';
+    next unless $self->playable($card);
+    if ($card->is_conversion) {
+      push @menu, map {; [sprintf('Convert %s with card %u', $_, $i), $i-1]}
+        $card->conversion_list($self->cubes);
+    } else {
+      push @menu, ["Play ".$card->describe, $i-1];
+    }
+  }
+  print 'Cubes: ', $self->cubes; say '       Score: ', $self->score;
+  return @menu, @mktmenu, ['Reclaim used cards'];
+}
+
+sub play ($self, $menuchoice) {
+  croak 'play takes a menu choice' unless ref $menuchoice eq 'ARRAY';
+  my ($text, $index) = @$menuchoice;
+  my ($action) = split ' ', lc $text;
+  my $copy = $self->clone;
+  if ($action eq 'claim') {
+    my $card = splice($self->reward->@*, $index, 1);
+    push $self->reward->@*, shift $self->reward_deck->@*
+      if $self->reward_deck && $self->reward_deck->@*;
+    $self->spend($card->cost);
+    $self->scored($card->benefit);
+  } elsif ($action eq 'purchase') {
+    my $card = splice($self->market->@*, $index, 1);
+    push $self->market->@*, shift $self->deck->@* if $self->deck && $self->deck->@*;
+    push $self->hand->@*, $card;
+    my $mcubes = splice($self->market_cubes->@*, $index, 1);
+    push $self->market_cubes->@*, '';
+    $self->earn($mcubes);
+  } elsif ($action eq 'play' || $action eq 'convert') {
+    my $card = splice($self->hand->@*, $index, 1);
+    push $self->discard->@*, $card;
+    my ($cost, $benefit) = ($card->cost, $card->benefit);
+    if ($card->is_conversion) {
+      croak 'text unexpected' unless $text =~ /([YGB]+)->([GBP]+)/;
+      ($cost, $benefit) = ($1, $2);
+    }
+    $self->spend($cost);
+    $self->earn($benefit);
+  } elsif ($action eq 'reclaim') {
+    push $self->hand->@*, $self->discard->@*;
+    $self->discard([]);
+  }
+}
+
+
+
+sub sample {
+  my %gstate = (
+    cubes => 'yygb',
+    hand => [[113=>44],[44=>12333],[33=>1124],[33=>11222],[333=>444],[33=>224],[11=>3],[222=>333],[__=>11]],
+    discard => [[11]],
+    reward => [[ggbbpp=>19],[yyybb=>9],[ggbbb=>13],[pppp=>16],[gggpp=>14]],
+    market => [[gg=>1113],[p=>1113],[gg=>33],[4],[yyy=>222]],
+    market_cubes => ['y', ('')x4],
+  );
+  return __PACKAGE__->new(%gstate);
+}
+
+
+1;
+
